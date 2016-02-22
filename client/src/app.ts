8e31fa8ecaac97c58a9846f74ba0c53abeb4f4cd
Lukas Ruebbelke <simpul@gmail.com>
2016-02-22 10:19:51 -0700

    added todo list

--- client/src/app.ts
+++ client/src/app.ts
@@ -100,3 +100,19 @@ export class App {
     this.context.subscribe(c => console.log('this.context', c));
   }
 }
+
+//-------------------------------------------------------------------
+//  MASTER TODO LIST
+//-------------------------------------------------------------------
+
+// MAIN GOAL To simplify the semantics around platform so that they can be illustrated
+// in a simple proof of concept
+
+// Clearly show how user and session context is consumed at a component level if at all
+// Clearly show event flow from component to session
+// Clearly show how changing user context will affect state down to the component level
+
+// Create action registry
+// Populate action component for each capability
+// Emit appropriate action upon selection
+// Demonstrate the appropriate action being handled at a service / store level
